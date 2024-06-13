a42255ece001ca7013a375020dd1acbe5cb97044
ahmed14allam <ahmed144allam@gmail.com>
2024-06-13 11:39:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+
+print("welcom to the youtube channel name generator:")
+nameqwe = (input("what is yiur channel about?\n"))
+nameasd = (input("what is your nickname?\n"))
+print("you could name your channel")
+print(nameqwe + " with " + nameasd)
